06c8e299cffe7d6b23b21855f8c7947394b374af
youssefwaheed123 <youssefwaheed@gmail.com>
2024-08-18 01:30:56 +0300

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next.js 15 here we go!</h1>
-      <h2 className="h2-bold">Next.js 15 here we go!</h2>
-      <h3 className="h3-bold">Next.js 15 here we go!</h3>
-    </div>
-  );
-}
